f1a9c347e42ef2885ebeb16eb35cb70c78f2777a
uvpatel <uvpatel7271@gmail.com>
2024-10-31 21:30:28 -0700

    Update code2.py

--- chapter-1-ps/code2.py
+++ chapter-1-ps/code2.py
@@ -1,6 +1,12 @@
 # 3. Install an external module and use it to perform an operation of your interest. 
 
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("I will speak this text") # this module is use for speech racognize
-engine.runAndWait()
+
+
+import pyttsx3  # Importing the 'pyttsx3' module, which is used for text-to-speech conversion
+
+engine = pyttsx3.init()  # Initializing the text-to-speech engine
+
+# Setting the text that will be converted to speech
+engine.say("I will speak this text")  # The 'say' method queues the text for speech synthesis
+
+engine.runAndWait()  # The 'runAndWait' method processes the voice command and waits for it to finish speaking
