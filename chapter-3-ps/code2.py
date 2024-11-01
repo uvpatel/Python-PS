efd532c4f19212dc2a36d9bfd8aa2217056fc333
uvpatel <uvpatel7271@gmail.com>
2024-10-31 21:55:16 -0700

    Update code2.py

--- chapter-3-ps/code2.py
+++ chapter-3-ps/code2.py
@@ -1,9 +1,20 @@
 # 2. Write a program to fill in a letter template given below with name and date.
-letter = '''
-Dear Name,
-You are selected!
-<|Date|>'''
 
-name = input("Enter name: ")
-date = int(input("Enter date: "))
+# Template letter with placeholders for name and date
+
+letter = '''Dear <|Name|>,
+You are selected!
+<|date|>'''
+
+# Taking user input for name and date
+Name = input("Enter your name: ")
+date = input("Enter the date: ")
+
+# Replacing placeholders with user inputs
+letter = letter.replace("<|Name|>", Name)  # Replacing <|Name|> with the user's name
+letter = letter.replace("<|date|>", date)  # Replacing <|date|> with the user's provided date
+
+# Printing the final letter with the replacements
+print(letter)
+
 
