a9ba6875bcff351f546ad3c21ae56177110f38d4
uvpatel <uvpatel7271@gmail.com>
2024-10-31 22:00:45 -0700

    Update code5.py

--- chapter-3-ps/code5.py
+++ chapter-3-ps/code5.py
@@ -1,4 +1,6 @@
 # 5. Write a program to format the following letter using escape sequence characters.
+
 letter = "Dear Harry,\nthis python course is nice.\nThanks!"
+#  using escape sequence character of newline character.
 
 print(letter)
