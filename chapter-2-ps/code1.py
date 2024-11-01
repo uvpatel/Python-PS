23c84db7022e10921c66fd9a2a9fde7465de1284
uvpatel <uvpatel7271@gmail.com>
2024-10-31 21:34:30 -0700

    Update code1.py

--- chapter-2-ps/code1.py
+++ chapter-2-ps/code1.py
@@ -1,6 +1,11 @@
 # 1. Write a python program to add two numbers.
 
+
+#  Take input from user 
+
 a = float(input("Enter first number: "))
 b= float(input("Enter second number: "))
+
+# print sum.
 
 print("The sum of two given number is: ",a+b)
