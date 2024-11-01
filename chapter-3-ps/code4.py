b168247b413008e654a801df1f606d02801d9036
uvpatel <uvpatel7271@gmail.com>
2024-10-31 21:59:17 -0700

    Update code4.py

--- chapter-3-ps/code4.py
+++ chapter-3-ps/code4.py
@@ -1,7 +1,7 @@
 # 4. Replace the double space from problem 3 with single spaces.
 
-
+# User input.
 string = input("Enter string: ")
 
-
+#  This replace methods of given argument replace double space by single space.
 print(string.replace("  "," "))
