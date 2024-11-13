59c4804f8277ca9c88572dcb27df748e0ffa5530
uvpatel <uvpatel7271@gmail.com>
2024-11-12 22:15:30 -0800

    Update code2.py

--- chapter-4-ps/code2.py
+++ chapter-4-ps/code2.py
@@ -1,20 +1,34 @@
 # This problem same as code 1 
 
-marks = []
+try:
 
-marks_1 = float(input("Enter marks: "))
-marks.append(marks_1)
-marks_2 = float(input("Enter marks: "))
-marks.append(marks_2)
-marks_3 = float(input("Enter marks: "))
-marks.append(marks_3)
-marks_4 = float(input("Enter marks: "))
-marks.append(marks_4)
-marks_5 = float(input("Enter marks: "))
-marks.append(marks_5)
-marks_6 = float(input("Enter marks: "))
-marks.append(marks_6)
+    marks = []
 
-marks.sort # this sort methods print output in sorting menor.
+    marks_1 = float(input("Enter marks: "))
+    marks.append(marks_1)
+    marks_2 = float(input("Enter marks: "))
+    marks.append(marks_2)
+    marks_3 = float(input("Enter marks: "))
+    marks.append(marks_3)
+    marks_4 = float(input("Enter marks: "))
+    marks.append(marks_4)
+    marks_5 = float(input("Enter marks: "))
+    marks.append(marks_5)
+    marks_6 = float(input("Enter marks: "))
+    marks.append(marks_6)
+  # this sort method sort list items in ascending order.
+    marks.sort
 
-print(marks)
+    print(marks)
+except ValueError:
+    print("Input should be in number")    
+
+# sort() modifies the list directly.(defualt = ascending)
+# sorted() creates a new sorted list.
+# To sort in descending order, set reverse=True.
+
+
+# for Descending 
+# Descending order
+# sorted_numbers_desc = sorted(numbers, reverse=True)
+# print("Descending:", sorted_numbers_desc)
