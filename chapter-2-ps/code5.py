d3f921bb301703ae9825b4a6a3b50a5ba85ad667
uvpatel <uvpatel7271@gmail.com>
2024-11-12 21:55:08 -0800

    Update code5.py

--- chapter-2-ps/code5.py
+++ chapter-2-ps/code5.py
@@ -1,12 +1,17 @@
 # 5. Write a python program to find an average of two numbers entered by the user.
 
+
+try:
 # Taking input from users.
+    number_1 = float(input("Enter first number: "))
+    number_2 = float(input("Enter  second number: "))
 
-number_1 = float(input("Enter first number: "))
-number_2 = float(input("Enter  second number: "))
+    avg = (number_1+number_2)/2
+# Printing output.
+    print("The avrage of given number is: ",avg)
 
-avg = (number_1+number_2)/2
+except ValueError:
+    print("Input should be float or integer")    
 
-# printing average of given number.
-
-print("The avrage of given number is: ",avg)
+finally:
+    print("End")
