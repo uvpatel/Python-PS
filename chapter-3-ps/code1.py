bbd5752a1e017a8d92c3d75b842c6fd805487bbe
uvpatel <uvpatel7271@gmail.com>
2024-11-12 21:58:25 -0800

    Update code1.py

--- chapter-3-ps/code1.py
+++ chapter-3-ps/code1.py
@@ -1,6 +1,11 @@
 '''1. Write a python program to display a user entered name followed by Good
 Afternoon using input () function'''
 
-name = input("Enter your name: ")
+try:
+    name = str(input("Enter your name: "))
 
-print("Good Afternoon"+ name)
+    print(f"Good Afternoon {name}")
+except TypeError:
+    print("Input should be in string")   
+finally:
+    print("End of programme")     
