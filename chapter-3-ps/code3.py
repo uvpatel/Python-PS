981690c2eb52740e302deb6df5bef7c588afdddd
uvpatel <uvpatel7271@gmail.com>
2024-11-12 22:04:30 -0800

    Update code3.py

--- chapter-3-ps/code3.py
+++ chapter-3-ps/code3.py
@@ -1,7 +1,14 @@
 # 3. Write a program to detect double space in a string.
 
-string = input("Enter string: ")
+try:
+    string = input("Enter string: ")
 
 # This count methods count given argument double spaces.
+    print(f"The double spaces are present in this string is:{string.count("  ")}")
 
-print(string.count("  ")) 
+except ValueError:
+    print("No double space detected in this input")    
+except TypeError:
+  print("Output should be in integer")
+
+
