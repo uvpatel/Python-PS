e4eb496cc8c127f6f22f2cd4e52e9b91357d03fd
uvpatel <uvpatel7271@gmail.com>
2024-11-12 22:08:18 -0800

    Update code4.py

--- chapter-3-ps/code4.py
+++ chapter-3-ps/code4.py
@@ -1,7 +1,19 @@
 # 4. Replace the double space from problem 3 with single spaces.
 
-# User input.
-string = input("Enter string: ")
 
+
+
+
+try:
+# User input.
+    string = str(input("Enter string: "))
+
+#  how to apply: nameofvariable.replace("from","to")
 #  This replace methods of given argument replace double space by single space.
-print(string.replace("  "," "))
+    print(string.replace("  "," "))
+
+except TypeError:
+    print("Input should be in forms of string: ")    
+
+finally:
+    print("End")
