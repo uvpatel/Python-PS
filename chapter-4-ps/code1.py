7b61501a9a77e8885f42edc2caa1d8c63ba43f48
uvpatel <uvpatel7271@gmail.com>
2024-11-12 22:10:34 -0800

    Update code1.py

--- chapter-4-ps/code1.py
+++ chapter-4-ps/code1.py
@@ -1,21 +1,27 @@
 # 1. Write a program to store seven fruits in a list entered by the user.
 
-fruits = []
+try:
+    fruits = []
 
-f1 = input("Enter fruit name: ")
-fruits.append(f1)
-f2 = input("Enter fruit name: ")
-fruits.append(f2)
-f3 = input("Enter fruit name: ")
-fruits.append(f3)
-f4 = input("Enter fruit name: ")
-fruits.append(f4)
-f5 = input("Enter fruit name: ")
-fruits.append(f5)
-f6 = input("Enter fruit name: ")
-fruits.append(f6)
-f7 = input("Enter fruit name: ")
-fruits.append(f7)
+    f1 = str(input("Enter fruit name: "))
+    fruits.append(f1)
+    f2 = str(input("Enter fruit name: "))
+    fruits.append(f2)
+    f3 = str(input("Enter fruit name: "))
+    fruits.append(f3)
+    f4 = str(input("Enter fruit name: "))
+    fruits.append(f4)
+    f5 = str(input("Enter fruit name: "))
+    fruits.append(f5)
+    f6 = str(input("Enter fruit name: "))
+    fruits.append(f6)
+    f7 = str(input("Enter fruit name: "))
+    fruits.append(f7)
 
-print(fruits)
-      
+    print(fruits)
+
+except TypeError:
+    print("Input should be in string")
+
+finally:
+     print("End")
