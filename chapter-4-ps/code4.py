41d8cac6d7a8049c5df2130d9727bd12d7b0e535
uvpatel <uvpatel7271@gmail.com>
2024-11-12 22:18:59 -0800

    Update code4.py

--- chapter-4-ps/code4.py
+++ chapter-4-ps/code4.py
@@ -1,17 +1,21 @@
 # 4. Write a program to sum a list with 4 numbers
-
-number = [] #Empty list
+try:
+    number = [] #Empty list
 
 # Taking input from user:
-
-a = float(input("Enter number: "))
-number.append(a)
-c = float(input("Enter number: "))
-number.append(c)
-b = float(input("Enter number: "))
-number.append(b)
-d = float(input("Enter number: "))
-number.append(d)
+    a = float(input("Enter number: "))
+    number.append(a)
+    c = float(input("Enter number: "))
+    number.append(c)
+    b = float(input("Enter number: "))
+    number.append(b)
+    d = float(input("Enter number: "))
+    number.append(d)
 
 # Printing sum of list elements using sum fucuntion.
-print("The sum of list elements: ",sum(number))
+    print("The sum of list elements: ",sum(number))
+except TypeError:
+    print("Input should be in number")   
+
+finally:
+  print("End")
