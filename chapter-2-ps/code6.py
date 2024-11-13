259974afcb9128e21e7253e30b84686c9c5ef67d
uvpatel <uvpatel7271@gmail.com>
2024-11-12 21:55:43 -0800

    Update code6.py

--- chapter-2-ps/code6.py
+++ chapter-2-ps/code6.py
@@ -1,5 +1,12 @@
 # 6. Write a python program to calculate the square of a number entered by the user.
 
-number = float(input("Enter number: "))
+try:
+    number = float(input("Enter number: "))
 
-print("The squre of the given number is: ",number**2)
+    print(f"The squre of {number} is: {number**2}")
+    
+except TypeError:
+    print("Input should integer or floating point number")    
+
+finally:
+    print("End")    
